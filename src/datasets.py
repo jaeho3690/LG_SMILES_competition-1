5a256a90cbdc04b0d67dc9d9921b3b3e85fc51fa
jaeho <jaeho3690@gmail.com>
2020-09-07 19:31:05 +0900

    edit model.py

--- src/datasets.py
+++ src/datasets.py
@@ -23,36 +23,33 @@ class SmilesDataset(Dataset):
         # Open hdf5 file where images are stored
         self.h = h5py.File(data_folder/ f"{self.split}_IMAGES_{base_file_name}.hdf5", 'r')
         self.imgs = self.h['images']
-        # Load encoded captions (completely into memory)
+        # Load encoded sequences (completely into memory)
         with open(data_folder/ f"{self.split}_SMILES_SEQUENCES_{base_file_name}.json", 'r') as j:
-            self.captions = json.load(j)
+            self.sequences = json.load(j)
 
-        # Load caption lengths (completely into memory)
+        # Load sequence lengths (completely into memory)
         with open(data_folder/ f"{self.split}_SMILES_SEQUENCE_LENS_{base_file_name}.json", 'r') as j:
-            self.caplens = json.load(j)
+            self.sequence_lens = json.load(j)
 
         # PyTorch transformation pipeline for the image (normalizing, etc.)
         self.transform = transform
 
         # Total number of datapoints
-        self.dataset_size = len(self.captions)
+        self.dataset_size = len(self.sequences)
 
     def __getitem__(self, i):
         img = torch.FloatTensor(self.imgs[i] / 255.)
         if self.transform is not None:
             img = self.transform(img)
 
-        caption = torch.LongTensor(self.captions[i])
+        sequence = torch.LongTensor(self.sequences[i])
 
-        caplen = torch.LongTensor([self.caplens[i]])
+        sequence_len = torch.LongTensor([self.sequence_lens[i]])
 
-        if self.split is 'TRAIN':
-            return img, caption, caplen
+        if self.split in {'TRAIN', 'VAL'}:
+            return img, sequence, sequence_len
         else:
-            # We need to figure out if there is more to return depending on the score type in Validation,  Test
-            all_captions = torch.LongTensor(
-                self.captions[((i // self.cpi) * self.cpi):(((i // self.cpi) * self.cpi) + self.cpi)])
-            return img, caption, caplen, all_captions
+            return img
 
     def __len__(self):
         return self.dataset_size
