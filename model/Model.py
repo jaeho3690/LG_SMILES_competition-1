208a48b98b9242baee846a15ea9ba663df34cead
HongJun Yang <yhj0428@unist.ac.kr>
2020-09-30 22:54:08 +0900

    test fix

--- model/Model.py
+++ model/Model.py
@@ -197,7 +197,7 @@ class MSTS:
 
             while not is_smiles:
                 self._seed_everything(self._seed + add_seed)
-                imgs = self._encoder(imgs.unsqueeze_(0))
+                imgs = self._encoder(imgs.unsqueeze(0))
                 predictions = self._decoder(imgs, self._decode_length)
 
                 SMILES_predicted_sequence = list(torch.argmax(predictions.detach().cpu(), -1).numpy())[0]
